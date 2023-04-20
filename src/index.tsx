f8896bd9a55c9287549369d39deda3463a1fa746
Ruslan Volovenko <vokoloven@gmail.com>
2023-04-20 18:54:14 +0300

    Deleted basename

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ root.render(
     <StrictMode>
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
-                <BrowserRouter basename="pet-shelter-app">
+                <BrowserRouter>
                     <App />
                 </BrowserRouter>
             </PersistGate>

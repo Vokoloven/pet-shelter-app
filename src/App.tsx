8aa3b264c8744709c133ccd4bb303e252b273cbd
Ruslan Volovenko <vokoloven@gmail.com>
2023-04-22 18:54:08 +0300

    Test check

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,8 @@ export const App = () => {
     const dispatch = useDispatch<AppDispatch>()
     const isFirstRender = useRef<boolean>(true)
 
+    console.log('Test')
+
     useAccess()
     useEffect(() => {
         if (isFirstRender.current) {
